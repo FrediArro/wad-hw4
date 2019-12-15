db51c8b22ff94572d0f1e8b808fe95f2674b1028
fredi <arro.fredi@gmail.com>
2019-12-15 23:33:25 +0200

    Added solutions to task 1 and 4

--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -1,19 +1,29 @@
 import {mount} from '@vue/test-utils'
 import List from "../../src/components/List.vue";
 
+
 describe('List', () => {
 
     const wrapper = mount(List);
 
     // 1
     it('component displays correct text if no item provided', () => {
-        expect(wrapper.html()).toContain('<h3>Add your first Todo task</h3>')
+        if(wrapper.vm.sortedList.length === 0) {
+            expect(wrapper.html()).toContain('<h3>Add your first Todo task</h3>');
+        }
     });
 
 
     // 4
     it('item is marked as done then item is updated correctly', () => {
-        expect(1).toEqual(1)
+        const sortedList = wrapper.vm.sortedList;
+        if (sortedList.length > 0) {
+        const task = sortedList[0];
+        const status = task.done;
+        const button = wrapper.find("span:first-of-type");
+        button.trigger("click");
+        expect(task.done).toBe(!status);
+        }
     });
 
 });
