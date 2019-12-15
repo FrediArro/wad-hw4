506ff402673c268e10fe63d9f46179ccef970f65
gerlik <gerliks@gmail.com>
2019-12-15 16:59:29 +0200

    Finished 2nd test - Header component displays correct date, year, month and weekday

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -4,10 +4,25 @@ import Header from "../../src/components/Header.vue";
 describe('Header component', () => {
 
     const wrapper = mount(Header);
+    let today = new Date();
 
     // 2
-    it('displays correct date, year, month, weekday', () => {
-        expect(1).toEqual(1)
+    it('displays correct date', () => {
+        expect(wrapper.vm.day).toEqual(today.getDate())
+    });
+
+    it('displays correct year', () => {
+        expect(wrapper.vm.year).toEqual(today.getFullYear())
+    });
+
+    it('displays correct month', () => {
+        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        expect(wrapper.vm.month).toEqual(months[today.getMonth()])
+    });
+
+    it('displays correct weekday', () => {
+        let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        expect(wrapper.vm.weekday).toEqual(weekdays[today.getDay()])
     });
 
 });
